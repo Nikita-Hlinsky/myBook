1e8085c3134812575ff648aed1c1eb022e4a64d9
Nikita-Hlinsky <super.nikita0308@gmail.com>
2021-07-07 18:07:44 +0300

    CardList - Card: fixes bugs

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
 import './App.css';
-import Anime from './components/Anime'
+import CardList from "./components/organisms/CardList"
+
+import WW from "./img/walterWhite.png"
 
 function App() {
+
+  const listOfChars = [{
+    "imgUrl": {WW},
+    "fullName": "Walter White",
+    "liveStasus": "Live",
+    "birthDate": "12.11.2001"
+  }]
+  // const fullName = "Walter White"
+  // const liveStasus = "Live"
+  // const birthDate = "12.11.2001"
+
   return (
     <div className="App">
-      <h1>Anim pics1</h1>
-      <Anime />
+      <h1>123</h1>
+      <CardList listOfChars={listOfChars} />
     </div>
   );
 }
