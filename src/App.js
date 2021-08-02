2f7d3dd496bc69981eca8c74357dad3b1cdd923c
NikitaHlinsky-0308 <nikatwork0099@gmail.com>
2021-08-03 01:34:06 +0300

    CardList - done

--- src/App.js
+++ src/App.js
@@ -19,7 +19,28 @@ function App() {
     birthDate: '09-24-1984',
     imgUrl: 'https://vignette.wikia.nocookie.net/breakingbad/images/9/95/JesseS5.jpg/revision/latest?cb=20120620012441',
     liveStatus: 'Alive',
-  }
+  },
+  {
+    id: uuidv4(),
+    fullName: 'Skyler White',
+    birthDate: '08-11-1970',
+    imgUrl: 'https://s-i.huffpost.com/gen/1317262/images/o-ANNA-GUNN-facebook.jpg',
+    liveStatus: 'Alive',
+  },
+  {
+    id: uuidv4(),
+    fullName: 'Walter White Jr.',
+    birthDate: '07-08-1993',
+    imgUrl: 'https://media1.popsugar-assets.com/files/thumbor/WeLUSvbAMS_GL4iELYAUzu7Bpv0/fit-in/1024x1024/filters:format_auto-!!-:strip_icc-!!-/2018/01/12/910/n/1922283/fb758e62b5daf3c9_TCDBRBA_EC011/i/RJ-Mitte-Walter-White-Jr.jpg',
+    liveStatus: 'Alive',
+  },
+  {
+    id: uuidv4(),
+    fullName: 'Henry Schrader',
+    birthDate: 'Unknown',
+    imgUrl: 'https://vignette.wikia.nocookie.net/breakingbad/images/b/b7/HankS5.jpg/revision/latest/scale-to-width-down/700?cb=20120620014136',
+    liveStatus: 'Deceased',
+  },
   ]
 
   return (
